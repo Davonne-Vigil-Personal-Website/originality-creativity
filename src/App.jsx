666d3b87a63aefa355bf3df389dea1209fc1a9ab
Davonne Vigil <davonnejv@gmail.com>
2025-09-05 01:17:24 -0500

    did this work, second portfolio project idea

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-pink-300">Goodnight everybody...!</h1>
+      <h1 className="text-blue-600 font-bold text-4xl">
+        Mic check one two, one two...!
+      </h1>
     </>
   );
 }
--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function Hobbies() {
+export default function Interest
+() {
   return <div></div>;
 }
