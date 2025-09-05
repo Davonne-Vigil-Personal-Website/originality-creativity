4658fc7fe742bc46d4982ab73f7b1e016ff26593
Davonne Vigil <davonnejv@gmail.com>
2025-09-05 02:24:23 -0500

    start: start the UI, start About Me section..!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
+import AboutMe from "./components/AboutMe";
 
 function App() {
   return (
-    <>
-      <h1 className="text-blue-600 font-bold text-4xl">
-        Mic check one two, one two...!
-      </h1>
-    </>
+    <main>
+      <AboutMe />
+    </main>
   );
 }
 
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,3 +1,12 @@
+import Image from "./Image";
+
 export default function AboutMe() {
-  return <div></div>;
+  return (
+    <header>
+      <Image image="./images/davonne.png" className="max-w-xs rounded-xl" />
+      <h1 className="font-bold text-2xl">Davonne Vigil</h1>
+      <p>Female</p>
+      <p>31 years old</p>
+    </header>
+  );
 }
