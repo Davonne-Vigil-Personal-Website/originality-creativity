5d69749f81dfa7dca882be769f05beb95083c8c2
Davonne Vigil <davonnejv@gmail.com>
2025-09-07 22:14:49 -0500

    add location and last login

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import Image from "./Image";
 
 export default function AboutMe() {
+  const [user, setUser] = useState({
+    name: "Davonne Vigil",
+    gender: "Female",
+    age: 31,
+    city: "Arlington",
+    state: "Texas",
+    country: "United States",
+    image: "./images/davonne.png",
+  });
+
+  const todaysDate = new Date().toLocaleDateString();
+
   return (
     <section className="p-8">
       <div className="flex flex-col md:flex-row gap-8">
         <div className="flex flex-col gap-4">
-          <h1 className="font-bold text-2xl">Davonne Vigil</h1>
-          <Image image="./images/davonne.png" className="max-w-xs rounded-xl" />
+          <h1 className="font-bold text-2xl">{user.name}</h1>
+          <Image image={user.image} className="max-w-xs rounded-xl" />
         </div>
+
         <div className="flex flex-col md:mt-20">
-          <p>Female</p>
-          <p>31 years old</p>
+          <p>{user.gender}</p>
+          <p>{user.age} years old</p>
+          <br />
+          <p>{user.city}</p>
+          <p>{user.state}</p>
+          <p>{user.country}</p>
         </div>
+
+        <p>Last Login: {todaysDate}</p>
       </div>
     </section>
   );
