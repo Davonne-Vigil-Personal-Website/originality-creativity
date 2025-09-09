f1ff0208c1972f5d871875390655b39d5ae326f9
Davonne Vigil <davonnejv@gmail.com>
2025-09-08 23:39:32 -0500

    feature: second column component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import SecondColumn from "./components/SecondColumn";
 
 function App() {
   return (
     <main>
       <Header />
-      <AboutMe />
+      <div className="flex flex-col lg:flex-row justify-center items-center">
+        <AboutMe />
+        <SecondColumn />
+      </div>
     </main>
   );
 }
---
+++ src/components/SecondColumn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SecondColumn() {
+  return (
+    <section>
+      <div className="border-2 p-6 max-w-6xl w-full">
+        <p className="font-bold ">Davonne is in your extended network</p>
+      </div>
+    </section>
+  );
+}
