6d8ebb0866d539c98ed071a1fe64a022fe200c6a
Davonne Vigil <davonnejv@gmail.com>
2025-09-11 00:15:56 -0500

    add projects, map over each project with styles

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,24 @@
 export default function Experience() {
   const myExperience = [
-    { project: "Design to Dev 2025", link: "", id: 0 },
-    { project: "For His Purpose Notary", link: "", id: 1 },
-    { project: "HabitQuest", link: "", id: 2 },
+    { project: "Davonne's Disney Inspired Portfolio", link: "", id: 0 },
+    { project: "Design to Dev 2025", link: "", id: 1 },
+    { project: "For His Purpose Notary", link: "", id: 2 },
+    { project: "HabitQuest", link: "", id: 3 },
+    { project: "Our Space", link: "", id: 4 },
   ];
   return (
     <section className="mt-8">
-      <h2 className="font-mono text-lg font-bold">Davonne's Latest Work</h2>
-      {myExperience.map((experience) => (
-        <section>
-          <p>{experience.project}</p>
-        </section>
-      ))}
+      <h2 className="font-mono text-lg font-bold">Davonne's Latest Work:</h2>
+      <ul className="flex flex-col gap-8 mt-8">
+        {myExperience.map((experience, id) => (
+          <li key={id}>
+            {experience.project}
+            <a href={experience.link} className="font-bold font-mono">
+              (view more)
+            </a>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
