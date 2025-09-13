7a038c912fb40b3b3eaf2b5ac03fb86ce90cc0d4
Davonne Vigil <davonnejv@gmail.com>
2025-09-12 21:55:02 -0500

    update Second Colum Davonne is in your network section, add basketball image

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -20,7 +20,9 @@ export default function Experience() {
   ];
   return (
     <section className="mt-8 px-4 sm:px lg:px-12">
-      <h2 className="font-mono text-2xl font-bold">Davonne's Latest Work:</h2>
+      <h2 className="bg-[url('./images/hoop.jpg)] font-mono text-2xl font-bold">
+        Davonne's Latest Work:
+      </h2>
       <ul className="flex flex-col gap-8 mt-8">
         {myExperience.map((experience, id) => (
           <li key={id} className="text-xl">
--- src/components/SecondColumn.jsx
+++ src/components/SecondColumn.jsx
@@ -3,8 +3,8 @@ import Experience from "./Experience";
 export default function SecondColumn() {
   return (
     <section className="w-full">
-      <div className="border-2 p-4 lg:p-6 lg:mt-20">
-        <p className="font-bold w-full text-center">
+      <div className="border-2 p-4 lg:p-6 lg:mt-20 bg-[url('./images/hoop3.jpg')] bg-cover bg-center">
+        <p className="font-bold text-center mx-auto bg-black w-fit text-white p-1 rounded">
           Davonne is in your extended network
         </p>
       </div>
