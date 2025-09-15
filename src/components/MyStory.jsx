c4d8f82873b96ca4d2cf9fde08724feee848acd4
Davonne Vigil <davonnejv@gmail.com>
2025-09-14 20:47:05 -0500

    update: code, clean up code, map over array of objects for my about me, skills and education

--- src/components/MyStory.jsx
+++ src/components/MyStory.jsx
@@ -1,25 +1,37 @@
-import React from "react";
-
 export default function MyStory() {
-  const myStory = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed cursus lectus. Phasellus faucibus eget magna at vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget mollis sem. Etiam tempus suscipit sagittis. Suspendisse tempor est non ipsum dictum interdum. Duis nec eros nunc. Integer velit nisi`;
+  const myStory = [
+    {
+      sectionName: "About Me",
+      details: `Vestibulum ultricies quis urna vitae consectetur. Donec dignissim vehicula metus non varius. Vestibulum ornare dictum erat a rhoncus. Sed lobortis suscipit leo vel imperdiet. Duis porta dui vitae mauris venenatis blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. `,
+      id: 0,
+    },
+    {
+      sectionName: "Skills",
+      details: `blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah`,
+      id: 1,
+    },
+    {
+      sectionName: "Education",
+      details: `blah blah blah blah blah blah blah blah blah blah blah blah`,
+      id: 2,
+    },
+  ];
   return (
     <section className="w-full mt-8">
       <h2 className="bg-blue-400 text-xl w-full font-mono mb-4 p-2">
         From Sales to Coding
       </h2>
 
-      <h3 className="font-mono text-xl">About Me:</h3>
-      <p>{myStory}</p>
-
-      <div className="mt-8">
-        <h3 className="font-mono text-xl">Education:</h3>
-        <p>{myStory}</p>
-      </div>
-
-      <div className="mt-8">
-        <h3 className="font-mono text-xl">Goals:</h3>
-        <p>{myStory}</p>
-      </div>
+      {myStory.map((story) => (
+        <section key={story.id} className="p-2">
+          <div className="flex flex-col">
+            <h3 className="text-2xl font-mono">{story.sectionName}</h3>
+            <ul>
+              <li>{story.details}</li>
+            </ul>
+          </div>
+        </section>
+      ))}
     </section>
   );
 }
