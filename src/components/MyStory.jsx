c9d1e7ca2844d80ae370e903e4b1063cd7510ec4
Davonne Vigil <davonnejv@gmail.com>
2025-09-19 23:15:14 -0500

    work on education section, add array of objectsts to show school details

--- src/components/MyStory.jsx
+++ src/components/MyStory.jsx
@@ -2,23 +2,43 @@ export default function MyStory() {
   const myStory = [
     {
       sectionName: "About Me",
-      details: `Vestibulum ultricies quis urna vitae consectetur. Donec dignissim vehicula metus non varius. Vestibulum ornare dictum erat a rhoncus. Sed lobortis suscipit leo vel imperdiet. Duis porta dui vitae mauris venenatis blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. `,
+      details: `Vestibulum ultricies quis urna vitae consectetur. Donec dignissim vehicula metus non varius. Vestibulum ornare dictum erat a rhoncus. Sed lobortis suscipit leo vel imperdiet. Duis porta dui vitae mauris venenatis blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus.`,
       id: 0,
     },
     {
       sectionName: "Skills",
-      details: `blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah`,
+      details: `blah blah blah blah`,
       id: 1,
     },
     {
       sectionName: "Education",
-      details: `blah blah blah blah blah blah blah blah blah blah blah blah`,
+      details: [
+        {
+          school: "University of Texas at Arlington ",
+          year: "2014-2017",
+          degree: "General Studies",
+          info: "blah blah blah",
+        },
+        {
+          school: "Tarrant County College",
+          year: "2018-2019",
+          degree: "Associate of Arts of Science, Library Technician",
+          info: "blah blah blah",
+        },
+        {
+          school: "Fullstack Academy",
+          year: " 2023",
+          degree: "Certificate in Web Development",
+          info: "blah blah blah",
+        },
+      ],
       id: 2,
     },
   ];
+
   return (
     <section className="w-full mt-8 bg-white shadow-md">
-      <h2 className="bg-#808402 text-white bg-[#28231D] text-xl w-full font-mono mb-4 p-2 rounded-t-lg">
+      <h2 className="bg-[#28231D] text-white text-xl w-full font-mono mb-4 p-2 rounded-t-lg">
         From Sales to Coding
       </h2>
 
@@ -26,9 +46,21 @@ export default function MyStory() {
         <section key={story.id} className="p-4">
           <div className="flex flex-col">
             <h3 className="text-2xl font-mono">{story.sectionName}</h3>
-            <ul>
-              <li>{story.details}</li>
-            </ul>
+
+            {Array.isArray(story.details) ? (
+              <ul className="p-2 flex flex-col gap-8">
+                {story.details.map((item, index) => (
+                  <li key={index} className="list-disc">
+                    <p className="font-semibold text-xl">{item.school}</p>
+                    <p className="italic text-lg">{item.year}</p>
+                    <p className=" italic text-lg">{item.degree}</p>
+                    <p className="text-gray-600 text-lg">{item.info}</p>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="mt-2">{story.details}</p>
+            )}
           </div>
         </section>
       ))}
