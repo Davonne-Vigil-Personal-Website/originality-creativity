3ad971fcd3933fed1165f30a9aa00c58081dcdd3
Davonne Vigil <davonnejv@gmail.com>
2025-09-20 17:47:06 -0500

    update Error.jsx for mobile resonsiveness

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,13 +5,13 @@ export default function Error() {
     <section className="flex flex-col lg:flex-row bg-pink-50">
       <div className="flex-1 flex flex-col justify-center py-8 items-center text-center ">
         <div className="lg:mb-96">
-          <h1 className="text-9xl font-spicy text-pink-500 drop-shadow-sm">
+          <h1 className="text-8xl md:text-9xl font-spicy text-pink-500 drop-shadow-sm">
             404
           </h1>
-          <p className="text-4xl font-medium text-gray-700">
+          <p className="text-2xl md:text-4xl font-medium text-gray-700">
             Oops! Page not found.
           </p>
-          <p className="mt-2 text-gray-500 text-2xl max-w-lg">
+          <p className="mt-2 text-gray-500 text-xl md:text-2xl max-w-lg">
             It looks like the page you’re looking for doesn’t exist.
           </p>
         </div>
